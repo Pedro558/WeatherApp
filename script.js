68e19a44c7ccffedfa4605f6aaf904f7774413f0
pedro558 <pedroafonso109@gmail.com>
2023-01-06 20:18:39 -0300

    :sparkles: feat: Add displayWeather function

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ let weather = {
   fetchWeather: function(city) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => this.displayWeather(data))
+  },
+  displayWeather: function(data){
+    const { name } = data
+    const { description, icon } = data.weather[0]
+    const { temp, humidity } = data.main
+    const { speed } = data.wind
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = `Weather in ${name}`
+    document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${icon}.png`
+    document.querySelector('.description').innerText = description
+    document.querySelector('.temp').innerText = `${temp.toFixed(0)} °C`
+    document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`
+    document.querySelector('.wind').innerText = `Wind speed: ${speed}km/h`
   }
 }
