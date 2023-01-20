adb513b9f1b47b54683d69a2a484a29ed2f9c5cd
pedro558 <pedroafonso109@gmail.com>
2023-01-20 18:21:39 -0300

    :sparkles: feat: Elements.js grab html elements

---
+++ js/elements.js
@@ -0,0 +1,19 @@
+const city =  document.querySelector(".city")
+const icon =  document.querySelector('.icon')
+const description =  document.querySelector('.description')
+const temperature =  document.querySelector('.temp')
+const humidity = document.querySelector('.humidity')
+const wind = document.querySelector('.wind')
+const weather = document.querySelector('.weather')
+const body =  document.body
+
+export {
+  city,
+  icon,
+  description,
+  temperature,
+  humidity,
+  wind,
+  weather,
+  body
+}
