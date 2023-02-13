ee172693b12f2fb1d73eb869fe606441589bdbcf
pedro558 <pedroafonso109@gmail.com>
2023-02-13 01:57:26 -0300

    :sparkles: feat: add setTimeOut

--- js/script.js
+++ js/script.js
@@ -21,22 +21,25 @@ class SearchWeather{
     const { temp, humidity } = results.main
     const { speed } = results.wind
     
-    weatherElem.innerHTML = `
-    <h1 class="city">Weather in ${name}</h1>
+    setTimeout(() => {
+      weatherElem.innerHTML = `
+      <h1 class="city">Weather in ${name}</h1>
+        
+        <div class="temp-icon">
+          <h2 class="temp">${temp.toFixed(0)} °C</h2>
+          <img src="https://openweathermap.org/img/wn/${icon}.png" alt="weather_icon" class="icon"/>
+        </div>
+  
+        <div class="weather-text">
+          <div class="description">${description}</div>
+          <div class="humidity">Humidity: ${humidity}</div>
+          <div class="wind">Wind speed: ${speed}km/h</div>
+        </div>
+      `
+  
+      bodyElem.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ name +"')"
       
-      <div class="temp-icon">
-        <h2 class="temp">${temp.toFixed(0)} °C</h2>
-        <img src="https://openweathermap.org/img/wn/${icon}.png" alt="weather_icon" class="icon"/>
-      </div>
-
-      <div class="weather-text">
-        <div class="description">${description}</div>
-        <div class="humidity">Humidity: ${humidity}</div>
-        <div class="wind">Wind speed: ${speed}km/h</div>
-      </div>
-    `
-
-    bodyElem.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ name +"')"
+    }, 1500);
     
   }
 
